9c6ddda761ae95e47866bdcb6194547e60a101b4
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 17:12:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -98,25 +98,22 @@ def changing_alan
     }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
 end
 
 def adding_to_dennis
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
+	  {
+ 		  :grace_hopper => {
+ 		    :known_for => "COBOL",
+ 		    :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 end
